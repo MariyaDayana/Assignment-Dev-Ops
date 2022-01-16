2f48e6d3dfb9ecbbe3abfe90b2d8c8244f4031fd
MariyaDyana <mariyadayana629@gmail.com>
2022-01-16 11:45:43 +0530

    first commit

---
+++ Week10Assignment/src/main/java/com/hcl/greadlearning/week10assignment/main/DevopsMain.java
@@ -0,0 +1,11 @@
+package com.hcl.greadlearning.week10assignment.main;
+
+public class DevopsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dev-Ops");
+
+	}
+
+}
